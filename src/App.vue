a7d9aef555908a720200ad3913c947b1974507ca
Tannaz <tannaz.teymourian@gmail.com>
2023-02-22 21:49:41 +0330

    Changes to be committed:

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
+    <particles/>
     <FirstClass msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+
 import FirstClass from './components/FirstClass.vue'
+import particles from "./components/particles"
 
 export default {
   name: 'App',
   components: {
     FirstClass,
+    particles,
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import ParticlesJs from './components/ParticlesJs.vue'
 
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new*/
 new Vue({
   render: h => h(App),
   el: "#app",
